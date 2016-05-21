4b353cc584983c8870865c2c7128c88c737a651b
Florian Krüger <florian.krueger@projectserver.org>
2016-05-21 09:46:52 +0200

    initial commit

---
+++ Sources/Pandora.h
@@ -0,0 +1,19 @@
+//
+//  Pandora.h
+//  Pandora
+//
+//  Created by Florian Krüger on 21/05/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pandora
+FOUNDATION_EXPORT double PandoraVersionNumber;
+
+//! Project version string for Pandora
+FOUNDATION_EXPORT const unsigned char PandoraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pandora/PublicHeader.h>
+
+
