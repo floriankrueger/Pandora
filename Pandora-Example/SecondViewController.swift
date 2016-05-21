e3e0f817459ddffaa4a93b0ea54b5d2a82b0a40c
Florian Krüger <florian.krueger@projectserver.org>
2016-05-21 10:59:08 +0200

    adds target setup

---
+++ Pandora-Example/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pandora-Example
+//
+//  Created by Florian Krüger on 21/05/16.
+//
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
